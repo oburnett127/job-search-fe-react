13a03a5feec75dc402f4c183a7922d112523dace
Owen Burnett <oburnett127@gmail.com>
2023-02-20 19:03:31 -0600

    Modified job service. Trying to use react query to get the job details for a job.

--- src/pages/JobDetail.js
+++ src/pages/JobDetail.js
@@ -2,17 +2,18 @@ import JobItem from '../components/JobItem';
 import { useQuery } from 'react-query';
 import JobService from '../services/JobService';
 import { useParams } from 'react-router-dom';
+import axios from "axios";
 
 function JobDetailPage() {
   const { jobId } = useParams();
 
   console.log(jobId);
 
-  const { data: jobData } = useQuery(["job", jobId], () => JobService.getJob(jobId));
+  const { data: jobData } = useQuery('job', () => JobService.getJobById(jobId));
 
   console.log(jobData);
 
-  return <JobItem job={jobData.data} />;
+  return <JobItem job={jobData} />;
 }
 
 export default JobDetailPage;
